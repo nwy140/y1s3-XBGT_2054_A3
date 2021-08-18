97e17b8ad2eb4dd5367e5c6fe8e3ba013e24acd2
nwy140-Win11 <nwy140@gmail.com>
2021-08-18 23:50:50 +0800

    A3 V0.4 Movement2D Ability

--- A3/Assets/MyScripts/_Unit/Components/Abilities/AbilityCompMotionGroundEvade2D.cs
+++ A3/Assets/MyScripts/_Unit/Components/Abilities/AbilityCompMotionGroundEvade2D.cs
@@ -5,6 +5,7 @@ public class AbilityCompMotionGroundEvade2D : AbilityBaseComp
 {
     [Header("AbilityComp")]
     public float evadeAcelSpeed = 1000f;
+    public AnimationCurve evadeAnimCurve;
     protected override void Awake()
     {
         eAbilityTechniques = EAbilityTechniques.GroundEvade2D;
@@ -23,10 +24,10 @@ public class AbilityCompMotionGroundEvade2D : AbilityBaseComp
         originalRot = _ownerUnitRefs.transform.rotation;
         _ownerUnitRefs._unitCharacterController._rigidbody2D.velocity = Vector2.zero;
         // same for both Ai and Player
-        if (buttonDown)
-        {
-            _ownerUnitRefs._unitCharacterController._rigidbody2D.AddRelativeForce(evadeAcelSpeed * Vector2.up);
-        }
+        //if (buttonDown)
+        //{
+        //    _ownerUnitRefs._unitCharacterController._rigidbody2D.AddRelativeForce(evadeAcelSpeed * Vector2.up);
+        //}
     }
     public override void OnAbilityActiveStay()
     {
@@ -35,14 +36,35 @@ public class AbilityCompMotionGroundEvade2D : AbilityBaseComp
         {
             _ownerUnitRefs.transform.rotation = originalRot;
         }
+
+        _ownerUnitRefs._unitCharacterController._rigidbody2D.AddRelativeForce(evadeAcelSpeed * Vector2.up * Time.deltaTime);
     }
     public override void OnAbilityActiveExit()
     {
         base.OnAbilityActiveExit();
-        _ownerUnitRefs._unitCharacterController._rigidbody2D.velocity = originalVelocitySize * transform.up;
+        if (isEvadeAnimCurveActive == false)
+        {
+            isEvadeAnimCurveActive = true;
+            StartCoroutine(AbilityWithAnimCurve());
+        }
         originalRot = Quaternion.identity;
     }
 
+    public float elpasedTime = 0f;
+    public bool isEvadeAnimCurveActive;
+    IEnumerator AbilityWithAnimCurve()
+    {
+        var duration = activeDuration;
+        while (elpasedTime < duration)
+        {
+            elpasedTime += Time.deltaTime;
+            float strength = evadeAnimCurve.Evaluate(elpasedTime / duration);
+            _ownerUnitRefs._unitCharacterController._rigidbody2D.AddRelativeForce(evadeAcelSpeed * Vector2.up * strength);
+
+            isEvadeAnimCurveActive = false;
+            yield return null;
+        }
+    }
     //public override IEnumerator OnAbilityStarted()
     //{
     //    AbilityFunctionality();
